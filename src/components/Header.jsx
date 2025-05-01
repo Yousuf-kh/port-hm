36e753a0ad0ec660ace3488933b3291488bc38f8
Ziyovuddin995 <madiyorovziyovuddin043@gmail.com>
2025-05-01 14:32:24 +0500

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,22 +8,27 @@ const Header = () => {
     const [menu, setMenu] = useState(false)
 
     return (
-        <header className='max-w-[1110px] b-r w-full mx-auto fixed top-0 bg-[#1B1B1B] left-1/2 transform -translate-x-1/2 z-10 mb-[70px]'>
-            <div className="container1 flex justify-between items-center h-[70px] text-[#9C9C9C]">
-                <nav aria-label="Main navigation" className='hidden md:flex gap-[70px] text-[14px]'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/portfolio'>Portfolio</Link>
-                    <Link to='/contact'>Contact</Link>
-                </nav>
+        <>
+            <header className='max-w-[1110px] b-r w-full mx-auto fixed top-0 bg-[#1B1B1B] left-1/2 transform -translate-x-1/2 z-10 mb-[70px]'>
+                <div className="container1 flex justify-between items-center h-[70px] text-[#9C9C9C]">
+                    <nav aria-label="Main navigation" className='hidden md:flex gap-[70px] text-[14px]'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/portfolio'>Portfolio</Link>
+                        <Link to='/contact'>Contact</Link>
+                    </nav>
 
-                <div className="flex gap-[25px] text-[22px]">
-                    <FaLinkedinIn className='cursor-pointer' />
-                    <FaBehance className='cursor-pointer' />
-                    <FaTwitter className='cursor-pointer' />
+                    <div className="flex gap-[25px] text-[22px]">
+                        <FaLinkedinIn className='cursor-pointer' />
+                        <FaBehance className='cursor-pointer' />
+                        <FaTwitter className='cursor-pointer' />
+                    </div>
+
+                    <IoMdMenu onClick={() => { setMenu(true) }} className='text-2xl cursor-pointer md:hidden' />
                 </div>
 
-                <IoMdMenu onClick={() => { setMenu(true) }} className='text-2xl cursor-pointer md:hidden' />
-            </div>
+
+
+            </header>
 
             <div className={`fixed w-full h-[100dvh] bg-[#222] top-0 menu ${menu ? 'active' : ''}`}>
                 <button
@@ -43,8 +48,7 @@ const Header = () => {
                     <Link onClick={() => setMenu(false)} to='/contact'>Contact</Link>
                 </nav>
             </div>
-
-        </header>
+        </>
     )
 }
 
